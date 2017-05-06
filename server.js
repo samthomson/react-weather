4593cfb23c832a156f2b38a2e9afb00cbad10e99
samthomson <samt@samt.st>
2017-05-06 16:25:09 +0200

    pre-deployment

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 
 // create our app
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next()
+    } else {
+        res.redirect('https://' + req.hostname + req.url)
+    }
+})
 
 app.use(express.static('public'));
 
