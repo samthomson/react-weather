5f9ad383600a5dd604ddaf96fcedb0e4bc5ffad4
samthomson <samt@samt.st>
2017-05-06 18:40:40 +0200

    styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,37 @@
 var React = require('react')
 var {IndexLink, Link} = require('react-router')
 
-var Nav = () => {
-    return (
-        <div>
-            <h2>Nav</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            &nbsp;
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>about</Link>
-            &nbsp;
-            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>examples</Link>
-        </div>
-    )
-}
+var Nav = React.createClass({
+    render: function () {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+                        <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>about</Link></li>
+                        <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>examples</Link></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather" />
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="get weather" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        )
+    },
+    onSearch: function (e) {
+        e.preventDefault()
+        alert('not implemented yet..')
+    }
+})
 
 module.exports = Nav
