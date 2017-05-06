3f5b84e38003afe5f36a48ad58dc0e32ac1d1c7d
samthomson <samt@samt.st>
2017-05-06 23:24:31 +0200

    some more..

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -44,7 +44,7 @@ var Weather = React.createClass({
         }
     },
     componentWillReceiveProps: function (newProps) {
-        var location = this.newProps.location
+        var location = newProps.location.query.location
 
         if (location && location.length > 0) {
             this.handleSearch(location)
