241a8c5e05c0be9c0fc5a8a2d0d3278089b81121
samthomson <samt@samt.st>
2017-05-06 20:47:55 +0200

    about page links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,15 @@ var About = (props) => {
     return (
         <div>
             <h3>About Component</h3>
-            <p>Welcome to the about page</p>
+            <p>tools I used:</p>
+            <ol>
+                <li>
+                    <a href="https://facebook.github.io/react">react</a>
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">open weather map API</a>
+                </li>
+            </ol>
         </div>
     )
 }
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,21 @@
 var React = require('react')
+var {Link} = require('react-router')
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Nairobi'>Nairobi</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Chengdu'>Chengdu</Link>
+                </li>
+            </ol>
+        </div>
+
     )
 }
 
